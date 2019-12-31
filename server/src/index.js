733813faf5213e7a98d7284ace6c45afc2483831
Gabriel Queiroz <gabrielqueirozzn@gmail.com>
2019-12-31 17:28:50 -0300

    Adicionar agendar

--- server/src/index.js
+++ server/src/index.js
@@ -1,16 +1,16 @@
-const { GraphQLServer, PubSub } = require('graphql-yoga');
-const path = require('path');
-const resolvers = require('./resolvers');
-const mongoose = require('mongoose');
+const { GraphQLServer, PubSub } = require("graphql-yoga");
+const path = require("path");
+const resolvers = require("./resolvers");
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/graphqlnode', {
+mongoose.connect("mongodb://localhost:27017/graphqlnode", {
   useNewUrlParser: true
 });
 
 const pubsub = new PubSub();
 
 const server = new GraphQLServer({
-  typeDefs: path.resolve(__dirname, 'schemas', 'schema.graphql'),
+  typeDefs: path.resolve(__dirname, "schemas", "schema.graphql"),
   resolvers,
   context: { pubsub }
 });
--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,30 +1,46 @@
 const User = require("./Models/User");
 const OneSignal = require("onesignal-node");
 const NEW_USER = "NEW_USER";
+const Agenda = require("agenda");
+const { MongoClient } = require("mongodb");
 const client = new OneSignal.Client(
   "6e929521-00bc-42b5-99a3-a48bff012616",
   "YmM4MWM5NDYtMjM5NC00ZTlmLTg5ZjQtMzNhODRhMGE5Y2Qz"
 );
 
 const sendNotification = async () => {
-  const notification = {
-    contents: {
-      pt: "Olha a notificação aeeeeee",
-      en: "olhaa  a notificação aeee"
-    },
-    included_segments: ["Active Users"]
-  };
-  try {
-    const response = await client.createNotification(notification);
-    console.log(response.body.id);
-    console.log("deu certo");
-  } catch (e) {
-    if (e instanceof OneSignal.HTTPError) {
-      // When status code of HTTP response is not 2xx, HTTPError is thrown.
-      console.log(e.statusCode);
-      console.log(e.body);
+  const mongoConnectionString = "mongodb://localhost:27017/agendatest";
+  const agenda = new Agenda({
+    db: { address: mongoConnectionString, collection: "jobs" }
+  });
+  // Define a "job", an arbitrary function that agenda can execute
+  agenda.define("hello", async () => {
+    const notification = {
+      contents: {
+        pt: "Olha a notificação aeeeeee",
+        en: "olhaa  a notificação aeee"
+      },
+      included_segments: ["Active Users"]
+    };
+    try {
+      const response = await client.createNotification(notification);
+      console.log(response.body.id);
+      console.log("deu certo");
+    } catch (e) {
+      if (e instanceof OneSignal.HTTPError) {
+        // When status code of HTTP response is not 2xx, HTTPError is thrown.
+        console.log(e.statusCode);
+        console.log(e.body);
+      }
     }
-  }
+  });
+
+  agenda.on("ready", function() {
+    agenda.schedule(new Date(Date.now() + 5000), "hello");
+    (async function() {
+      await agenda.start();
+    })();
+  });
 };
 
 module.exports = {
